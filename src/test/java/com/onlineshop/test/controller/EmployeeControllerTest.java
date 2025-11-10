d032803c64470c8de93917a2b74a7e1ccbba65b6
Sanjar <sanjar171766@gmail.com>
2025-11-10 21:02:12 +0500

    new branch

---
+++ src/test/java/com/onlineshop/test/controller/EmployeeControllerTest.java
@@ -0,0 +1,4 @@
+package com.onlineshop.test.controller;
+
+public class EmployeeControllerTest {
+}
---
+++ src/test/java/com/onlineshop/test/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.onlineshop.test.repository;
+
+public class EmployeeRepositoryTest {
+}
